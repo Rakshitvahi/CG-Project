9d10786987c2c40d53101437ce940a77edbdb210
Rakshit Vahi <vahi.rakshit@gmail.com>
2018-03-14 19:10:22 +0530

    Loading page and Welcome page

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<GL/glut.h>>
+#include<GL/glut.h>
+#include<time.h>
+#include<string.h>
 
-static int flag=0;
+int f=0;
+int a=200,b=0;
+int i=0,state=0;
+float o1=1,o2=1;
+
 void typeText(char *string,float x,float y,float z,float w,float h,float sx,float sy,int width){
     int i;
 	glColor3f(x, y, z);
@@ -19,83 +25,105 @@ void typeText(char *string,float x,float y,float z,float w,float h,float sx,floa
 	glPopMatrix();
 	glFlush();
 }
-void rec(int x1,int y,int x2,int y2){
-    glColor3f(1,0,0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(x1,y);
-    glVertex2i(x2,y);
-    glVertex2i(x2,y2);
-    glVertex2i(x1,y2);
+
+//The following function is to provide a delay
+void delay(unsigned int mseconds)
+{
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+}
+
+/*The following function is use to get the blue bar to show loading in blue color
+where 'a' is the value of x which is incremented in steps of 10*/
+void setpoly(int a)
+{
+    glColor3f(0,0,1);
+    glBegin(GL_POLYGON);
+    glVertex2i(195,244);
+    glVertex2i(a,244);
+    glVertex2i(a,256);
+    glVertex2i(195,256);
     glEnd();
-
 }
-void keys(unsigned char key,int x,int y){
-    if(key ==(char)13){
-        printf("Hi");
-        flag=1;
-        display();
-        glutPostRedisplay();
 
+/*The following function is use to make the loading bar frame and to call setpoly() function at an interval to show the
+the progress bar
+variable 'i' is used to keep the trace of number of steps*/
+void loading()
+{
+    /*if(i==0)
+    {
+            typeText("LOADING...",1,0,0,195,244,0.2,0.2,3);
+            delay(500000);
+           glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    }*/
+
+    float j=1;
+    for(i=0;i<10;i++)
+    {
+
+        glClearColor(j,1,1,1.0);
+        j=j-0.1;
+        glColor3f(0,0,0);
+        glBegin(GL_LINE_LOOP);
+        glVertex2i(195,244);
+        glVertex2i(280,244);
+        glVertex2i(280,256);
+        glVertex2i(195,256);
+        glEnd();
+        setpoly(a);
+        delay(1000000);
+        a=a+10;
+        glFlush();
+        glClear(GL_COLOR_BUFFER_BIT);
     }
+    state=2;
+    glutPostRedisplay();
+}
+
+//The following function is used to show the screen after the loading is complete
+void screen2()
+{
+    glClearColor(0.1,1,1,1.0);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    delay(100000);
+    typeText("WELCOME",1,1,0,195,244,0.2,0.2,3);
+    delay(3000000);
+    state=3;
+    glFlush();
+    glutPostRedisplay();
+
 
 }
-void screen2(){
-    glClearColor(1,1,1,1);
+void screen3()
+{
+   glClearColor(1,1,1,1);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    //printf("bye");
+}
 
-    glutSwapBuffers();
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    if(state==0)
+        loading();
+    else if(state==2)
+        screen2();
+    else if(state==3)
+        screen3();
     glFlush();
 }
-void display(){
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	if(flag == 0){
-        int len;
-        char string[]="NAVIGATION";
-    //    rec(1,1,100,100);
-        glPushMatrix();
-        //glLoadIdentity();
-        glTranslatef(270,300,0);
-        glScalef(2,2,1);
 
-        glColor3f(0.93, 0.27, 0.21);
-        glBegin(GL_POLYGON);
-        glVertex2f(30,23);
-        glVertex2f(40,25);
-        glVertex2f(45,35);
-        glEnd();
-        glColor3f(0.75, 0.22, 0.17);
-        glBegin(GL_POLYGON);
-        glVertex2f(45,17);
-        glVertex2f(40,25);
-        glVertex2f(45,35);
-        glEnd();
-        glutSwapBuffers();
-        glPopMatrix();
-        glFlush();
-        len=(int) strlen(string);
-        typeText(string,0.99, 0.75, 0.06,175,600,0.5,0.5,5);
-        typeText("ENTER TO START",0.97, 0.22, 0.22,175,400,0.3,0.3,3);
-        typeText("RAHUL.J 1PE15CS115",0.75, 0.93, 0.96,10,130,0.2,0.2,3);
-        typeText("RAKSHIT 1PE15CS119",0.75, 0.93, 0.96,10,80,0.2,0.2,3);
-    }else if(flag == 1){
-        screen2();
-    }
-}
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize(700,700);
-	glutCreateWindow("Navigation");
-    glutKeyboardFunc(keys);
-	glClearColor(0,0.4,0.52,1);
-	glOrtho(0,700,0,700,0,700);
-	glutDisplayFunc(display);
-
-	glEnable(GL_DEPTH_TEST);
-	glutMainLoop();
-	return 0;
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(1024,800);
+    glutCreateWindow("Navigation");
+    glutDisplayFunc(display);
+    glOrtho(0,500.0,0,500.0,0,500.0);
+    glClearColor(1.0,1.0,1.0,1.0);
+    //glutReshapeFunc(myReshape);
+    //glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
 }
-
